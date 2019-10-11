c9023148b2829b531227dc0b8669b41443cd4c3c
KimSunYoung <12150029@inha.edu>
2019-10-11 23:25:07 +0900

    <mainActivity> 버튼을 menu1로 옮김
    <menu2>
    spinner 글자 보이게 수정
    search 창에 enter키가 먹히지 않음
    element name 수정

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,23 +3,15 @@ package com.example.myapplication;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
 
 import android.view.MenuItem;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.view.View;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
     private FragmentManager fragmentManger;
-    private Animation fab_open, fab_close;
-    private Boolean isFabOpen = false;
-    private FloatingActionButton fab1, fab2, fab3, fab4;
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -57,72 +49,8 @@ public class MainActivity extends AppCompatActivity {
         fragmentManger = getSupportFragmentManager();
         fragmentManger.beginTransaction().replace(R.id.frame_layout, new menu1()).commit();
 
-        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
-        fab_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab1 = findViewById(R.id.fab1);
-        fab2 = findViewById(R.id.fab2);
-        fab3 = findViewById(R.id.fab3);
-        fab4 = findViewById(R.id.fab4);
 
-        fab2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                fragmentManger.beginTransaction().replace(R.id.frame_layout, new menu2()).commit();
-                anim();
-            }
-        });
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int id = view.getId();
-                switch (id) {
-                    case R.id.fab:
-                        anim();
-                        break;
-                    case R.id.fab1:
-                        anim();
-                        break;
-                    case R.id.fab2:
-                        anim();
-                        break;
-                    case R.id.fab3:
-                        anim();
-                        break;
-                    case R.id.fab4:
-                        anim();
-                        break;
-
-                }
-            }
-        });
-
-    }
-
-    public void anim() {
-
-        if (isFabOpen) {
-            fab1.startAnimation(fab_close);
-            fab2.startAnimation(fab_close);
-            fab3.startAnimation(fab_close);
-            fab4.startAnimation(fab_close);
-            fab1.setClickable(false);
-            fab2.setClickable(false);
-            fab3.setClickable(false);
-            fab4.setClickable(false);
-            isFabOpen = false;
-        } else {
-            fab1.startAnimation(fab_open);
-            fab2.startAnimation(fab_open);
-            fab3.startAnimation(fab_open);
-            fab4.startAnimation(fab_open);
-            fab1.setClickable(true);
-            fab2.setClickable(true);
-            fab3.setClickable(true);
-            fab4.setClickable(true);
-            isFabOpen = true;
-        }
     }
 
 
--- app/src/main/java/com/example/myapplication/menu1.java
+++ app/src/main/java/com/example/myapplication/menu1.java
@@ -8,22 +8,103 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class menu1 extends Fragment {
+
+    private Animation fab_open, fab_close;
+    private FloatingActionButton fab, fab1, fab2, fab3, fab4;
+    private Boolean isFabOpen = false;
 
     public menu1() {
         // Required empty public constructor
 
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu1, container, false);
+        View v= inflater.inflate(R.layout.fragment_menu1, container, false);
+
+        fab_open = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.fab_close);
+
+        fab = v.findViewById(R.id.fab);
+        fab1 = v.findViewById(R.id.fab1);
+        fab2 = v.findViewById(R.id.fab2);
+        fab3 = v.findViewById(R.id.fab3);
+        fab4 = v.findViewById(R.id.fab4);
+
+        fab2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                getFragmentManager().beginTransaction().replace(R.id.frame_layout, new menu2()).commit();
+                anim();
+            }
+        });
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int id = view.getId();
+                switch (id) {
+                    case R.id.fab:
+                        anim();
+                        break;
+                    case R.id.fab1:
+                        anim();
+                        break;
+                    case R.id.fab2:
+                        anim();
+                        break;
+                    case R.id.fab3:
+                        anim();
+                        break;
+                    case R.id.fab4:
+                        anim();
+                        break;
+
+                }
+            }
+        });
+        return v;
+    }
+
+    public void anim() {
+        if (isFabOpen) {
+            fab1.startAnimation(fab_close);
+            fab2.startAnimation(fab_close);
+            fab3.startAnimation(fab_close);
+            fab4.startAnimation(fab_close);
+            fab1.setClickable(false);
+            fab2.setClickable(false);
+            fab3.setClickable(false);
+            fab4.setClickable(false);
+            isFabOpen = false;
+        } else {
+            fab1.startAnimation(fab_open);
+            fab2.startAnimation(fab_open);
+            fab3.startAnimation(fab_open);
+            fab4.startAnimation(fab_open);
+            fab1.setClickable(true);
+            fab2.setClickable(true);
+            fab3.setClickable(true);
+            fab4.setClickable(true);
+            isFabOpen = true;
+        }
     }
 
 
