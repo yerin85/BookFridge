e9fd6819b7c812e263a13c0fff331ec7fbc0e635
KimSunYoung <yonci131101@gmail.com>
2019-11-07 02:10:50 +0900

    getLibrary 함수 추가

--- app/src/main/java/com/example/myapplication/LibraryMenu.java
+++ app/src/main/java/com/example/myapplication/LibraryMenu.java
@@ -1,13 +1,22 @@
 package com.example.myapplication;
 
 
-import android.os.Bundle;
-
 import android.app.Fragment;
-
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.myapplication.data.LibraryData;
+import com.example.myapplication.data.LibraryResponse;
+import com.example.myapplication.network.RetrofitClient;
+import com.example.myapplication.network.ServiceApi;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 /**
@@ -15,7 +24,7 @@ import android.view.ViewGroup;
  */
 public class LibraryMenu extends Fragment {
     UserInfo userInfo;
-
+    ServiceApi service;
 
     public LibraryMenu() {
         // Required empty public constructor
@@ -35,6 +44,22 @@ public class LibraryMenu extends Fragment {
         View v= inflater.inflate(R.layout.fragment_library_menu, container, false);
 
         userInfo = (UserInfo)getArguments().getSerializable("userInfo");
+        service = RetrofitClient.getClient().create(ServiceApi.class);
+
+        service.getLibrary(userInfo.userId).enqueue(new Callback<ArrayList<LibraryResponse>>() {
+            @Override
+            public void onResponse(Call<ArrayList<LibraryResponse>> call, Response<ArrayList<LibraryResponse>> response) {
+                ArrayList<LibraryResponse> libData = response.body();
+                for(int i=0;i<libData.size();i++){
+                    //여기서 뷰에 보여주기
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<LibraryResponse>> call, Throwable t) {
+
+            }
+        });
 
         // Inflate the layout for this fragment
         return v;
--- app/src/main/java/com/example/myapplication/network/ServiceApi.java
+++ app/src/main/java/com/example/myapplication/network/ServiceApi.java
@@ -9,11 +9,13 @@ import retrofit2.http.Query;
 import com.example.myapplication.NewItemResponse;
 import com.example.myapplication.data.BasicResponse;
 import com.example.myapplication.data.LibraryData;
+import com.example.myapplication.data.LibraryResponse;
 import com.example.myapplication.data.MyPageData;
 import com.example.myapplication.data.UserPrivateData;
 import com.example.myapplication.data.WishlistData;
+import com.example.myapplication.data.WishlistResponse;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public interface ServiceApi {
     @GET("ItemList.aspx?ttbkey=ttb0318592203001&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=JS&Version=20131101")
@@ -22,14 +24,23 @@ public interface ServiceApi {
     @POST("/user/saveLibrary")
     Call<BasicResponse> saveLibrary(@Body LibraryData data);
 
-    @GET("user/getLibrary")
-    Call<List<LibraryData>> getLibrary(@Query("userId") String userId);
+    @POST("/user/updateLibrary")
+    Call<BasicResponse> updateLibrary(@Body LibraryData data);
+
+    @POST("user/getLibrary")
+    Call<ArrayList<LibraryResponse>> getLibrary(@Query("userId") String userId);
+
+    @POST("/user/deleteLibrary")
+    Call<BasicResponse> deleteLibrary(@Query("userId") String userId,@Query("isbn") String isbn);
 
     @POST("/user/saveWishlist")
     Call<BasicResponse> saveWishlist(@Body WishlistData data);
 
-    @GET("user/getWishlist")
-    Call<List<WishlistData>> getWishlist(@Query("userId") String userId);
+    @POST("user/getWishlist")
+    Call<ArrayList<WishlistResponse>> getWishlist(@Query("userId") String userId);
+
+    @POST("/user/deleteWishlist")
+    Call<BasicResponse> deleteWishlist(@Query("userId") String userId,@Query("isbn") String isbn);
 
     @POST("/user/createUserPrivate")
     Call<BasicResponse> createUserPrivate(@Body UserPrivateData data);
