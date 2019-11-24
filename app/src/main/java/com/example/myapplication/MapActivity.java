50aeac71bd35e0510f59f7bc09fe3192fad43403
kimy0 <kimy9985@gmail.com>
2019-11-25 00:18:20 +0900

    교보, 영풍문고 좌표 입력

--- app/src/main/java/com/example/myapplication/MapActivity.java
+++ app/src/main/java/com/example/myapplication/MapActivity.java
@@ -51,7 +51,7 @@ public class MapActivity extends AppCompatActivity implements MapView.CurrentLoc
     String ypItemId;
     String ypUrl;
 
-        public class info{
+    public class info{
         String name; //매장명
         double lati; //위도
         double longi; //경도
@@ -105,14 +105,6 @@ mapPoints – 화면에 모두 보여주고자 하는 지도 좌표 리스트*/
 
     @Override
     public void onLocationChanged(Location location) {
-        mapPoint = MapPoint.mapPointWithGeoCoord(location.getLatitude(), location.getLongitude());
-        mapView.setMapCenterPoint(mapPoint, true);
-        mapView.removePOIItem(currentMarker);
-        currentMarker.setItemName("현재 위치");
-        currentMarker.setMapPoint(mapPoint);
-        currentMarker.setMarkerType(MapPOIItem.MarkerType.YellowPin);
-        mapView.addPOIItem(currentMarker);
-        mapView.selectPOIItem(currentMarker,true);
 
         for(int i =0; i<kyobodatas.size();i++){
             mapPoint = MapPoint.mapPointWithGeoCoord(kyobodatas.get(i).lati, kyobodatas.get(i).longi);
@@ -133,6 +125,15 @@ mapPoints – 화면에 모두 보여주고자 하는 지도 좌표 리스트*/
             mapView.addPOIItem(marker);
             mapView.selectPOIItem(marker,true);
         }
+
+        mapPoint = MapPoint.mapPointWithGeoCoord(location.getLatitude(), location.getLongitude());
+        mapView.setMapCenterPoint(mapPoint, true);
+        mapView.removePOIItem(currentMarker);
+        currentMarker.setItemName("현재 위치");
+        currentMarker.setMapPoint(mapPoint);
+        currentMarker.setMarkerType(MapPOIItem.MarkerType.RedPin);
+        mapView.addPOIItem(currentMarker);
+        mapView.selectPOIItem(currentMarker,true);
     }
 
     @Override
@@ -302,87 +303,87 @@ mapPoints – 화면에 모두 보여주고자 하는 지도 좌표 리스트*/
         kyobodatas.add(new info("천호",37.540486, 127.124961 ));
         kyobodatas.add(new info("청량리", 37.580705, 127.047652));
         kyobodatas.add(new info("합정",37.550056, 126.911926));
-//        kyobodatas.add(new info("가천대",));
-//        kyobodatas.add(new info("광교점",));
-//        kyobodatas.add(new info("광교월드스퀘어",));
-//        kyobodatas.add(new info("부천",));
-//        kyobodatas.add(new info("분당점",));
-//        kyobodatas.add(new info("성균관대",));
-//        kyobodatas.add(new info("송도",));
-//        kyobodatas.add(new info("인천",));
-//        kyobodatas.add(new info("일산",));
-//        kyobodatas.add(new info("판교",));
-//        kyobodatas.add(new info("평촌",));
-//
-//        kyobodatas.add(new info("경성대ㆍ부경대",));
-//        kyobodatas.add(new info("광주상무",));
-//        kyobodatas.add(new info("대구",));
-//        kyobodatas.add(new info("대전",));
-//        kyobodatas.add(new info("반월당",));
-//        kyobodatas.add(new info("부산",));
-//        kyobodatas.add(new info("세종",));
-//        kyobodatas.add(new info("센텀시티",));
-//        kyobodatas.add(new info("울산",));
-//        kyobodatas.add(new info("전북대",));
-//        kyobodatas.add(new info("전주",));
-//        kyobodatas.add(new info("창원",));
-//        kyobodatas.add(new info("천안",));
-//        kyobodatas.add(new info("칠곡",));
-//        kyobodatas.add(new info("포항공대점",));
-//        kyobodatas.add(new info("해운대",));
+        kyobodatas.add(new info("가천대",37.449507, 127.127715));
+        kyobodatas.add(new info("광교점",37.287488, 127.058179));
+        kyobodatas.add(new info("광교월드스퀘어",37.292577, 127.048151));
+        kyobodatas.add(new info("부천",37.484107, 126.782724));
+        kyobodatas.add(new info("분당점",37.383449, 127.122618));
+        kyobodatas.add(new info("성균관대",37.294057, 126.972508));
+        kyobodatas.add(new info("송도",37.381513, 126.657908));
+        kyobodatas.add(new info("인천",37.446157, 126.700996));
+        kyobodatas.add(new info("일산",37.642948, 126.789933));
+        kyobodatas.add(new info("판교",37.392710, 127.111994));
+        kyobodatas.add(new info("평촌",37.390057, 126.949925));
+
+        kyobodatas.add(new info("경성대ㆍ부경대",35.137676, 129.101711));
+        kyobodatas.add(new info("광주상무",35.152176, 126.848947));
+        kyobodatas.add(new info("대구",35.870549, 128.594609));
+        kyobodatas.add(new info("대전",36.352722, 127.379860));
+        kyobodatas.add(new info("반월당",35.866326, 128.590731));
+        kyobodatas.add(new info("부산",35.151895, 129.059608));
+        kyobodatas.add(new info("세종",36.496371, 127.263087));
+        kyobodatas.add(new info("센텀시티",35.169845, 129.131099));
+        kyobodatas.add(new info("울산",35.541277, 129.338702));
+        kyobodatas.add(new info("전북대",35.845790, 127.128100));
+        kyobodatas.add(new info("전주",35.819476, 127.145167));
+        kyobodatas.add(new info("창원",35.223499, 128.680727));
+        kyobodatas.add(new info("천안",36.819233, 127.155275));
+        kyobodatas.add(new info("칠곡",35.943972, 128.562366));
+        kyobodatas.add(new info("포항공대점",36.012868, 129.320553));
+        kyobodatas.add(new info("해운대",35.169914, 129.176013));
 
 
     }
 
     public void Youngpung(){
 
-        youngpungdatas.add(new info("홍대입구", 37.556988, 126.923409));
-        youngpungdatas.add(new info("종로본점",37.569679, 126.981991));
-//        youngpungdatas.add(new info("종로본점",));
+        youngpungdatas.add(new info("가산마리오",37.478601, 126.885153 ));
+        youngpungdatas.add(new info("강남역",37.499177, 127.027529));
+        youngpungdatas.add(new info("강남포스코",37.505871, 127.055804));
 
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
-//        youngpungdatas.add(new info("종로본점",));
+        youngpungdatas.add(new info("경산이마트",35.834630, 128.720367));
+        youngpungdatas.add(new info("광복롯데",35.097984, 129.036127));
+        youngpungdatas.add(new info("광주터미널",35.160348, 126.879341));
+        youngpungdatas.add(new info("구리롯데아울렛",37.611897, 127.140533));
+        youngpungdatas.add(new info("구미롯데마트",36.113763, 128.365376));
+        youngpungdatas.add(new info("군산롯데아울렛",35.976096, 126.738330));
+        youngpungdatas.add(new info("김포공항롯데",37.566783, 126.802505));
+        youngpungdatas.add(new info("대구대백",35.869901, 128.595839));
+        youngpungdatas.add(new info("대전터미널",36.351442, 127.437320));
+        youngpungdatas.add(new info("마산롯데",35.201495, 128.573422));
+
+        youngpungdatas.add(new info("목포터미널",34.812733, 126.417201));
+        youngpungdatas.add(new info("미아롯데",37.608314, 127.028873));
+        youngpungdatas.add(new info("부산남포",35.098653, 129.029547));
+        youngpungdatas.add(new info("부산대",35.232170, 129.083919));
+        youngpungdatas.add(new info("부산하단",35.106801, 128.966996));
+        youngpungdatas.add(new info("분당서현",37.383023, 127.121429));
+        youngpungdatas.add(new info("분당오리",37.340834, 127.106664));
+        youngpungdatas.add(new info("세종",36.495476, 127.262672));
+        youngpungdatas.add(new info("수원NC",37.250211, 127.019922));
+        youngpungdatas.add(new info("스타필드고양",37.647486, 126.896365));
+
+        youngpungdatas.add(new info("스타필드시티위례",37.479884, 127.148427));
+        youngpungdatas.add(new info("스타필드코엑스몰",37.511582, 127.059597));
+        youngpungdatas.add(new info("스타필드하남",37.545743, 127.224034));
+        youngpungdatas.add(new info("신림포도몰",37.483992, 126.930185));
+        youngpungdatas.add(new info("여의도IFC몰",37.525992, 126.925829));
+        youngpungdatas.add(new info("왕십리역",37.562171, 127.037923));
+        youngpungdatas.add(new info("용산아이파크몰",37.528867, 126.964055));
+        youngpungdatas.add(new info("위례",37.472917, 127.142830));
+        youngpungdatas.add(new info("유성",36.358754, 127.344112));
+        youngpungdatas.add(new info("의정부신세계",37.735403, 127.046987));
+
+        youngpungdatas.add(new info("인천스퀘어원",37.442585, 126.702508));
+        youngpungdatas.add(new info("인천터미널",37.442585, 126.702508));
+        youngpungdatas.add(new info("전주터미널",35.834641, 127.128749));
+        youngpungdatas.add(new info("종각종로",37.569824, 126.982259));
+        youngpungdatas.add(new info("죽전이마트",37.325775, 127.109630));
+        youngpungdatas.add(new info("진주",35.164465, 128.127278));
+        youngpungdatas.add(new info("천안불당",36.813416, 127.105776));
+        youngpungdatas.add(new info("청주",36.626746, 127.431979));
+        youngpungdatas.add(new info("포항남구",36.012756, 129.348911));
+        youngpungdatas.add(new info("홍대",37.556942, 126.923369));
 
     }
 }
